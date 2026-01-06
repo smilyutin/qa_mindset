63292d4f2534b503ec22e635c96407063b0baf48
smilyutin <smilyutin1@gmail.com>
2026-01-06 15:16:03 -0800

    Replace BrowserRouter with HashRouter for improved routing consistency

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Modules from './app/routes/Modules';
 import ModuleDetail from './app/routes/ModuleDetail';
 
-const basename = import.meta.env.BASE_URL || '/';
 
 const Router = () => (
-  <BrowserRouter basename={basename}>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Modules />} />
       <Route path="/modules/:id" element={<ModuleDetail />} />
       <Route path="*" element={<Modules />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
