5dadd9a320e464d08c94658c2927615032b6163b
smilyutin <smilyutin1@gmail.com>
2026-01-06 10:31:40 -0800

    g

--- src/router.tsx
+++ src/router.tsx
@@ -3,10 +3,11 @@ import Modules from './app/routes/Modules';
 import ModuleDetail from './app/routes/ModuleDetail';
 
 const Router = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/qa_mindset">
     <Routes>
       <Route path="/" element={<Modules />} />
       <Route path="/modules/:id" element={<ModuleDetail />} />
+      <Route path="*" element={<Modules />} />
     </Routes>
   </BrowserRouter>
 );
